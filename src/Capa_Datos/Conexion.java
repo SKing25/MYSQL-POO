b7e82058a2725e46df1c75474ecec1abe09b0739
SKing25 <162246363+SKing25@users.noreply.github.com>
2025-02-17 20:25:06 -0500

    creacion de 3 capas xd

---
+++ src/Capa_Datos/Conexion.java
@@ -0,0 +1,36 @@
+package Capa_Datos;
+import java.sql.*;
+
+public class Conexion {
+    private final String url = "jdbc:mysql://localhost:3306/ventas22";
+    private final String usuario = "root";
+    private final String pwd = "";
+
+    public Conexion() {}
+
+    public ResultSet Listar(String Cad){
+        try{
+            Connection cn = DriverManager.getConnection(url, usuario, pwd);
+            PreparedStatement da = cn.prepareStatement(Cad);
+            ResultSet tbl = da.executeQuery();
+            return tbl;
+        }
+        catch (SQLException e) {
+            javax.swing.JOptionPane.showMessageDialog(null, e.getMessage());
+            return null;
+        }
+    }
+
+    public String Ejecutar(String Cad){
+        try{
+            Connection cn = DriverManager.getConnection(url, usuario, pwd);
+            PreparedStatement da = cn.prepareStatement(Cad);
+            int r = da.executeUpdate();
+            return "Se afectaron " + r + " filas";
+        }
+        catch (SQLException e) {
+            javax.swing.JOptionPane.showMessageDialog(null, e.getMessage());
+            return "Error " + e.getMessage();
+        }
+    }
+}
---
+++ src/Capa_Negocio/DataArticulo.java
@@ -0,0 +1,17 @@
+package Capa_Negocio;
+
+import Capa_Datos.Conexion;
+
+public class DataArticulo {
+    private String art_cod;
+    private String art_nom;
+    private String art_uni;
+    private String art_pre;
+    private int art_stk;
+    private String art_marca;
+
+    public String EliminarArticulo() {
+        Conexion objmod = new Conexion();
+        String cad = "delete from articulo where art_cod ='" + this.getArt_cod()
+    }
+}
